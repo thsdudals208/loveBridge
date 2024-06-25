d2fae12e2c0c67c05af4644b304f9c22e6667461
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-06-25 15:45:56 +0900

    FIRST COMMIT

---
+++ src/main/java/com/jolipjo/lovebridge/LoveBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.jolipjo.lovebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jolipjo/lovebridge/LoveBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolipjo.lovebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
